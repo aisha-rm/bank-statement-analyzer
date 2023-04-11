6417fe427e62ded02e04332e65496e508aa70bdd
Aisha Mohammed <80531561+aisha-rm@users.noreply.github.com>
2023-04-11 23:51:46 +0100

    refactored to improve cohesion

---
+++ BankStatementProcessor.java
@@ -0,0 +1,54 @@
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankStatementProcessor {
+    
+    private final List<BankTransaction> bankTransactions;
+
+
+    public BankStatementProcessor(final List<BankTransaction> bankTransactions) {
+        this.bankTransactions = bankTransactions;
+    }
+    
+    
+    public double calculateTotalAmount() {
+        double total = 0;
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
+    }
+
+    public List<BankTransaction> selectInMonth (Month month) {
+        
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                bankTransactionsInMonth.add(bankTransaction);
+            }
+        }
+        return bankTransactionsInMonth;
+    }
+
+    public double calculateTotalInMonth(final Month month) {
+        double total = 0;
+        for(final BankTransaction bankTransaction:bankTransactions) {
+            if(bankTransaction.getDate().getMonth() == month) {
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
+    }
+
+    public double calculateTotalForCategory(final String category) {
+        double total = 0;
+        for(final BankTransaction bankTransaction:bankTransactions) {
+            if(bankTransaction.getDescription().equals(category)){
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
+    }
+}
+
--- BankTransactionAnalyzerSimple.java
+++ BankTransactionAnalyzerSimple.java
@@ -2,46 +2,67 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.time.Month;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
 
     private static final String RESOURCES = "statement.csv";
+    private static final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
 
     public static void main(String[] args) throws IOException {
-        
-        final Path path = Paths.get(RESOURCES);
+                
+        //final String fileName = args[0];
+        final Path path = Paths.get(RESOURCES); // + fileName);
         final List<String> lines = Files.readAllLines(path);
 
-        //get profit or loss for the statement
-        double total = 0d;
-        for(final String line : lines) {
-            final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
-        }
-        System.out.println("The total for all transactions is " + total);
+        final List<BankTransaction> bankTransactions 
+            = bankStatementParser.parseLinesFromCSV(lines);
 
-        int transactionCount = lines.size();
-        System.out.println("The total number of transactions is " + transactionCount);
+        final BankStatementProcessor bankStatementProcessor 
+            = new BankStatementProcessor(bankTransactions);
+
+            collectSummary(bankStatementProcessor);
+    }
+    
+    private static void collectSummary(final BankStatementProcessor bankStatementProcessor) {
+        System.out.println("The total for all transactions is "
+            + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("The total for transactions in March is " +
+            bankStatementProcessor.calculateTotalInMonth(Month.MARCH));
+        System.out.println("The total for transactions in April is " +
+            bankStatementProcessor.calculateTotalInMonth(Month.APRIL));
+        System.out.println("The total salary received is " +
+            bankStatementProcessor.calculateTotalForCategory("Salary"));
+    }        
+    
+}
+        // System.out.println("Total for transactions in April is " + 
+        // calculateTotalAmount(selectInMonth(bankTransactions, Month.APRIL)));
+
+        //get profit or loss for the statement
+        // double total = 0d;
+        // for(final String line : lines) {
+        //     final String[] columns = line.split(",");
+        //     final double amount = Double.parseDouble(columns[1]);
+        //     total += amount;
+        // }
+        // System.out.println("The total for all transactions is " + total);
+
+        // int transactionCount = lines.size();
+        // System.out.println("The total number of transactions is " + transactionCount);
 
 
         //get the sum for april
-        double aprilTotal =0;
-        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        for(final String line : lines) {
-            final String[] columns = line.split(",");
-            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
-            if(date.getMonth() == Month.APRIL) {
-                final double amount = Double.parseDouble(columns[1]);
-                aprilTotal += amount;
-            }
-        }
-        System.out.println("The total for transactions in April is " + aprilTotal);
-
-    }
-}
-
+    //     double aprilTotal =0;
+    //     final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    //     for(final String line : lines) {
+    //         final String[] columns = line.split(",");
+    //         final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+    //         if(date.getMonth() == Month.APRIL) {
+    //             final double amount = Double.parseDouble(columns[1]);
+    //             aprilTotal += amount;
+    //         }
+    //     }
+    //     System.out.println("The total for transactions in April is " + aprilTotal);
